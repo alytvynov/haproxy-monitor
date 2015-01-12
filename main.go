3a809ad5a088f2f46d2d097e71d3df7b93903b96
Andrew Lytvynov <lytvynov.a.v@gmail.com>
2015-01-12 10:46:05 -0800

    Add TCP interface for stats streaming.
    Add TCP listener that will forward relevant stat output to clients. Add
    basic pubsub mechanism between haproxy socket poller and network
    connections.
    Clients receive stat output in csv format identical to what haproxy
    sends on the socket with empty line indicating separation between
    states.

--- main.go
+++ main.go
@@ -2,28 +2,49 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"bytes"
+	"flag"
 	"log"
 	"net"
-	"os"
-	"strings"
 	"time"
 )
 
+var laddr = flag.String("laddr", ":8081", "address to listen on")
+
 func main() {
-	reconnect(os.Args[1])
+	flag.Parse()
+
+	datach := make(chan []byte)
+	go reconnect(flag.Arg(0), datach)
+	subch := make(chan subscriber)
+	go pubsub(datach, subch)
+
+	l, err := net.Listen("tcp", *laddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		con, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go serve(con, subch)
+	}
 }
 
-func reconnect(addr string) {
+func reconnect(addr string, out chan []byte) {
 	for {
 		log.Println("connecting to", addr)
-		if err := poll(addr); err != nil {
+		if err := poll(addr, out); err != nil {
 			log.Println(err)
 		}
 	}
 }
 
-func poll(addr string) error {
+func poll(addr string, out chan []byte) error {
 	uaddr, err := net.ResolveUnixAddr("unix", addr)
 	if err != nil {
 		return err
@@ -40,8 +61,9 @@ func poll(addr string) error {
 		return err
 	}
 
-	log.Println("> prompt")
 	time.Sleep(time.Second)
+
+	buf := &bytes.Buffer{}
 
 	s := bufio.NewScanner(con)
 	for {
@@ -49,25 +71,77 @@ func poll(addr string) error {
 		if err != nil {
 			return err
 		}
-		log.Println("> show stat")
 
 	printLoop:
 		for s.Scan() {
-			l := s.Text()
+			l := s.Bytes()
 			switch {
-			case l == "": // empty line is end of stat output
+			case len(l) == 0: // empty line is end of stat output
 				break printLoop
-			case strings.HasPrefix(l, ">"): // first line comment
-			case strings.HasPrefix(l, "stats"): // stats backends
+			case bytes.HasPrefix(l, []byte(">")): // first line comment
+			case bytes.HasPrefix(l, []byte("stats")): // stats backends
 			default:
-				fmt.Println("<", l)
+				buf.Write(append(l, '\n'))
 			}
 		}
+		buf.Write([]byte{'\n'})
+		out <- buf.Bytes()
+		buf.Reset()
 
 		if s.Err() != nil {
 			return s.Err()
 		}
 
 		time.Sleep(time.Second)
+	}
+}
+
+func serve(con net.Conn, sub chan subscriber) {
+	defer con.Close()
+
+	log.Println("new subscriber", con.RemoteAddr())
+	defer log.Println("subscriber disconnected", con.RemoteAddr())
+
+	in := make(chan []byte)
+	sub <- subscriber{id: con.RemoteAddr().String(), ch: in}
+	defer func() {
+		sub <- subscriber{id: con.RemoteAddr().String(), unsub: true}
+	}()
+	for stat := range in {
+		_, err := con.Write(stat)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+type subscriber struct {
+	id    string
+	unsub bool
+	ch    chan []byte
+}
+
+func pubsub(in chan []byte, sub chan subscriber) {
+	var data []byte
+	subs := make(map[string]chan []byte)
+
+	for {
+		select {
+		case data = <-in:
+			for _, s := range subs {
+				select {
+				case s <- data:
+				default:
+				}
+			}
+		case s := <-sub:
+			if s.unsub {
+				delete(subs, s.id)
+			} else {
+				subs[s.id] = s.ch
+			}
+			log.Println("len(subs):", len(subs))
+		}
 	}
 }
