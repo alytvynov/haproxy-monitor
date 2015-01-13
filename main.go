a001384dd9510902b3818208b236bd10962cb4f3
Andrew Lytvynov <lytvynov.a.v@gmail.com>
2015-01-12 18:38:49 -0800

    Implement basic interface with real data
    Should've split this commit into multiple smaller commits but too late.
    Following set of changes was made:
        - implement vertical split for multiple servers. Each server gets an
          equal part of vertical space and full horizontal space.
        - connect to actual servers and read data.
        - parse data: split stat chunks (on empty line), split fields of
          stats (as csv records).
        - add stat titles, pick only selected stats to print based on
          position. Right now it's a hardcoded slice of indexes and
          hardcoded slice of names. Might do something cleaner later on.
        - format stats by defining max field length.
        - add stat_indexes which lists positions of all stats. This will
          make it easier to select and add new stats to output.
        - blit and flush termbox buffer in dedicated goroutine. As per nsf's
          and Tv's suggestions, messing with termbox buffer is not
          thread-safe.
        - create helper view type. It's basically a combination of
          tulib.Buffer that holds contents of that part of screen and
          tulib.Rect that defines where on actual window that view is. It's
          important to remember that buffer's cells are not related to cells
          on actual window, they are blited via a separate goroutine.
        - add a bunch of helper methods on view for cleaner text drawing and
          updates.
        - write log into a file. This lets us get debug output without
          messing up the actual interface.
    
    This should be it but this was a massive commit so some other details
    could've been in here.

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/nsf/termbox-go"
 	"github.com/nsf/tulib"
 )
 
+const fieldCount = 63
+
+var (
+	fieldPos   = []int{0, 1, 4, 8, 9, 17}
+	fieldLen   = []int{17, 45, 6, 10, 10, 7}
+	fieldNames = []string{"group", "name", "scur", "bin", "bout", "status"}
+)
+
 func main() {
+	logf, err := os.Create("monitor.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logf.Close()
+	log.SetOutput(logf)
 	servers, err := parseConf("servers.conf")
 	if err != nil {
 		log.Fatal(err)
@@ -22,8 +41,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	window := tulib.TermboxBuffer()
-	centerLabel(&window, "termbox initialized, to exit press q")
+	drawch := make(chan view)
+	go draw(drawch)
+
+	w, h := termbox.Size()
+	bh := h / len(servers)
+	for i, s := range servers {
+		buf := tulib.NewBuffer(w, bh)
+		bufr := tulib.Rect{X: 0, Y: bh * i, Width: w, Height: bh}
+		go s.monitor(view{buf, bufr, drawch, make(chan struct{})})
+	}
 
 	for {
 		e := termbox.PollEvent()
@@ -39,6 +66,69 @@ func main() {
 			}
 		}
 	}
+}
+
+func draw(in chan view) {
+	for v := range in {
+		win := tulib.TermboxBuffer()
+		win.Blit(v.rect, 0, 0, &v.buf)
+		termbox.Flush()
+		v.done <- struct{}{}
+	}
+}
+
+type view struct {
+	buf  tulib.Buffer
+	rect tulib.Rect
+	draw chan view
+	done chan struct{}
+}
+
+func (v view) flush() {
+	v.draw <- v
+	<-v.done
+}
+
+func (v view) label(off int, text string, col termbox.Attribute) {
+	v.buf.DrawLabel(
+		tulib.Rect{Y: off, Width: v.buf.Width, Height: 1},
+		&tulib.LabelParams{Fg: col, Bg: termbox.ColorDefault},
+		[]byte(text))
+}
+
+func (v view) clear() {
+	v.buf.Fill(tulib.Rect{Y: 1, Width: v.buf.Width, Height: v.buf.Height - 2},
+		termbox.Cell{Bg: termbox.ColorDefault})
+}
+
+func (v view) title(text string) {
+	v.buf.DrawLabel(
+		tulib.Rect{Width: v.buf.Width, Height: 1},
+		&tulib.LabelParams{Fg: termbox.ColorBlack, Bg: termbox.ColorWhite},
+		[]byte(text))
+}
+
+func (v view) centerLabel(text string) {
+	v.center(text, termbox.ColorWhite, termbox.ColorBlack)
+}
+
+func (v view) centerError(text string) {
+	v.center(text, termbox.ColorWhite, termbox.ColorRed)
+}
+
+func (v view) center(text string, bg, fg termbox.Attribute) {
+	x := v.buf.Width/2 - len(text)/2
+	y := v.buf.Height / 2
+	v.buf.DrawLabel(
+		tulib.Rect{X: x, Y: y, Width: len(text), Height: 1},
+		&tulib.LabelParams{Fg: fg, Bg: bg},
+		[]byte(text),
+	)
+}
+
+func (v view) clearCenter() {
+	v.buf.Fill(tulib.Rect{Y: v.buf.Height / 2, Width: v.buf.Width, Height: 1},
+		termbox.Cell{Bg: termbox.ColorDefault})
 }
 
 type server struct {
@@ -65,13 +155,84 @@ func parseConf(path string) ([]server, error) {
 	return servers, s.Err()
 }
 
-func centerLabel(buf *tulib.Buffer, text string) {
-	x := buf.Width/2 - len(text)/2
-	y := buf.Height / 2
-	buf.DrawLabel(
-		tulib.Rect{X: x, Y: y, Width: len(text), Height: 1},
-		&tulib.LabelParams{Fg: termbox.ColorBlack, Bg: termbox.ColorWhite},
-		[]byte(text),
-	)
-	termbox.Flush()
+func (s server) monitor(v view) {
+	v.title(fmt.Sprintf("%s (%s)", s.name, s.addr))
+	v.buf.Fill(tulib.Rect{Width: v.buf.Width, Height: 1, Y: v.buf.Height - 1}, termbox.Cell{Bg: termbox.ColorDefault, Fg: termbox.ColorBlue, Ch: '-'})
+
+	for {
+		s.connectAndDraw(v)
+		time.Sleep(time.Second)
+	}
+}
+
+func (s server) connectAndDraw(v view) {
+	v.centerLabel("connecting")
+	v.flush()
+	time.Sleep(100 * time.Millisecond)
+	con, err := net.Dial("tcp", s.addr)
+	if err != nil {
+		v.clearCenter()
+		v.centerError("error: " + err.Error())
+		v.flush()
+		return
+	}
+	defer con.Close()
+
+	v.clearCenter()
+	v.flush()
+
+	scan := bufio.NewScanner(con)
+	buf := make([]byte, 0)
+	for scan.Scan() {
+		l := scan.Bytes()
+		if len(l) == 0 {
+			s.redraw(v, buf)
+			buf = buf[:0]
+			continue
+		}
+		buf = append(buf, append(l, '\n')...)
+	}
+}
+
+func (s server) redraw(v view, stats []byte) {
+	offs := 1 // offset from the top of buffer
+	s.drawStatTitles(v)
+	r := csv.NewReader(bytes.NewReader(stats))
+	for {
+		rec, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		offs++
+		if err != nil {
+			log.Println(err)
+			v.label(offs, err.Error(), termbox.ColorRed)
+			continue
+		}
+		if len(rec) != fieldCount {
+			err := fmt.Errorf("expected %d fields, got %d", fieldCount, len(rec))
+			log.Println(err)
+			v.label(offs, err.Error(), termbox.ColorRed)
+			continue
+		}
+		log.Println(rec, fieldPos)
+		v.label(offs, buildLine(rec, fieldPos), termbox.ColorWhite)
+	}
+	v.flush()
+}
+
+func buildLine(rec []string, pos []int) string {
+	l := ""
+	for i, j := range pos {
+		l += fmt.Sprintf(fmt.Sprintf("%%%ds|", fieldLen[i]), rec[j])
+	}
+	return l
+}
+
+func (s server) drawStatTitles(v view) {
+	l := ""
+	for i, n := range fieldNames {
+		l += fmt.Sprintf(fmt.Sprintf("%%%ds|", fieldLen[i]), n)
+	}
+	v.label(1, l, termbox.ColorCyan)
 }
